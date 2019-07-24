8e58c77431af646f39ddab1c72c337c9aaeb322c
mrsimonslice <mrsimonslice@gmail.com>
2019-07-24 08:07:07 +0700

    add Loading and NotFound page

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
+import Loading from './components/Loading'
 
-import Home from './containers/Home'
+const Home = Loadable({ loader: () => import('./containers/Home'), loading: Loading, })
+const NotFound = Loadable({ loader: () => import('./components/NotFound'), loading: Loading, })
 
 function Routes(props) {
     return (
         <Switch>
             <Route component={Home} exact path="/" />
+
+            { /* Finally, catch all unmatched routes */}
+            <Route component={NotFound} />
         </Switch>
     )
 }
